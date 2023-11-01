ad931a93615b81a866c81b5a268aee9120935eaa
Kyle Mangabat <kyle.mngbt@gmail.com>
2023-11-01 16:51:42 +0800

    Create makeRows function

--- javascript/app.js
+++ javascript/app.js
@@ -1,1 +1,11 @@
 const content = document.querySelector('.content');
+
+
+function makeRows(rows, cols) {
+  content.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
+  content.style.gridTemplateColumns = `repeat(${cols}, 1fr)`;
+  for (c = 0; c < (rows * cols); c++) {
+    let cell = document.createElement("div");
+    content.appendChild(cell).className = "grid-item";
+  };
+};
