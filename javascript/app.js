a72f61da31ea8c128223ad64adf10a3a6b3fb21f
Kyle Mangabat <kyle.mngbt@gmail.com>
2023-11-02 10:27:40 +0800

    Create normalMode function

--- javascript/app.js
+++ javascript/app.js
@@ -26,6 +26,16 @@ function rainbowMode() {
   })
 }
 
+function normalMode() {
+  const grids = document.querySelectorAll('.grid-item');
+  grids.forEach((grid) => {
+    grid.removeEventListener('mouseover', changeBgColor);
+  })
+  grids.forEach((grid) => {
+    grid.addEventListener('mouseover', changeBgColorRGB);
+  })
+}
+
 function changeBgColor() {
   this.style.backgroundColor = `black`;
 }
