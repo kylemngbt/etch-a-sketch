4ba006bed25302c35a35586e9d3b40c515743175
Kyle Soliveres <kyle.mngbt@gmail.com>
2024-08-24 15:10:44 +0800

    Implement the default 16x16 grid
    Using for loop inside a for loop, this will
    create the desired size of a grid using flexbox.

--- javascript/script.js
+++ javascript/script.js
@@ -1,2 +1,13 @@
 const container = document.querySelector('.container');
 
+  for (let i = 1; i <= 16; i++) {
+    const gridRow = document.createElement('div');
+    gridRow.classList.add('gridRow');
+    for (let i = 1; i <= 16; i++) {
+      const gridColumn = document.createElement('div');
+      gridColumn.classList.add('gridColumn');
+      gridRow.appendChild(gridColumn);
+    }
+    container.appendChild(gridRow);
+  }
+  
