82350463115f6a3b1482ea1ad9f02dec376e1380
Kyle Soliveres <kyle.mngbt@gmail.com>
2024-08-26 17:41:44 +0800

    Create the change grid size function

--- javascript/script.js
+++ javascript/script.js
@@ -16,4 +16,28 @@ const darkeningButton = document.querySelector('.darkening-button');
     }
     container.appendChild(gridRow);
   }
-  
+
+  function changeGridSize () {
+    let size;
+    do {
+      size = +prompt('Enter your desired grid size: (Minimum of 16, maximum of 100)');
+    } while (size < 16 || size > 100);
+    while(container.hasChildNodes()) {
+      container.removeChild(container.firstChild)
+    }
+
+    for (let i = 1; i <= size; i++) {
+      const gridRow = document.createElement('div');
+      gridRow.classList.add('gridRow');
+      for (let i = 1; i <= size; i++) {
+        const gridColumn = document.createElement('div');
+        gridColumn.classList.add('gridColumn');
+        gridRow.appendChild(gridColumn);
+        gridColumn.addEventListener('mouseover', () => {
+          gridColumn.classList.add('black');
+        })
+      }
+      container.appendChild(gridRow);
+    }
+
+  }
