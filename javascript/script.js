6bc233b02ba35fd6bc79eac6adf90d7ae0d09c54
Kyle Soliveres <kyle.mngbt@gmail.com>
2024-08-26 17:44:43 +0800

    Fix the indentation

--- javascript/script.js
+++ javascript/script.js
@@ -3,10 +3,33 @@ const changeGridButton = document.querySelector('.change-grid-button');
 const rgbButton = document.querySelector('.rgb-button');
 const darkeningButton = document.querySelector('.darkening-button');
 
+for (let i = 1; i <= 16; i++) {
+  const gridRow = document.createElement('div');
+  gridRow.classList.add('gridRow');
   for (let i = 1; i <= 16; i++) {
+    const gridColumn = document.createElement('div');
+    gridColumn.classList.add('gridColumn');
+    gridRow.appendChild(gridColumn);
+    gridColumn.addEventListener('mouseover', () => {
+      gridColumn.classList.add('black');
+    })
+  }
+  container.appendChild(gridRow);
+}
+
+function changeGridSize () {
+  let size;
+  do {
+    size = +prompt('Enter your desired grid size: (Minimum of 16, maximum of 100)');
+  } while (size < 16 || size > 100);
+  while(container.hasChildNodes()) {
+    container.removeChild(container.firstChild)
+  }
+
+  for (let i = 1; i <= size; i++) {
     const gridRow = document.createElement('div');
     gridRow.classList.add('gridRow');
-    for (let i = 1; i <= 16; i++) {
+    for (let i = 1; i <= size; i++) {
       const gridColumn = document.createElement('div');
       gridColumn.classList.add('gridColumn');
       gridRow.appendChild(gridColumn);
@@ -17,29 +40,8 @@ const darkeningButton = document.querySelector('.darkening-button');
     container.appendChild(gridRow);
   }
 
-  function changeGridSize () {
-    let size;
-    do {
-      size = +prompt('Enter your desired grid size: (Minimum of 16, maximum of 100)');
-    } while (size < 16 || size > 100);
-    while(container.hasChildNodes()) {
-      container.removeChild(container.firstChild)
-    }
+}
 
-    for (let i = 1; i <= size; i++) {
-      const gridRow = document.createElement('div');
-      gridRow.classList.add('gridRow');
-      for (let i = 1; i <= size; i++) {
-        const gridColumn = document.createElement('div');
-        gridColumn.classList.add('gridColumn');
-        gridRow.appendChild(gridColumn);
-        gridColumn.addEventListener('mouseover', () => {
-          gridColumn.classList.add('black');
-        })
-      }
-      container.appendChild(gridRow);
-    }
 
-  }
 
-  changeGridButton.addEventListener('click', changeGridSize);
+changeGridButton.addEventListener('click', changeGridSize);
